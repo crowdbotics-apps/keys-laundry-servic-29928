9fb81252882558dfb78627686772b650f86eb5f6
Crowdbotics <team@crowdbotics.com>
2021-08-18 20:42:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/keys_laundry_servic_29928/MainActivity.java
@@ -0,0 +1,15 @@
+package com.keys_laundry_servic_29928;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "keys_laundry_servic_29928";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "keys_laundry_servic_29928Identifier",
+  url: "https://keys_laundry_servic_29928Identifier.botics.co",
+  api: "https://keys_laundry_servic_29928Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
